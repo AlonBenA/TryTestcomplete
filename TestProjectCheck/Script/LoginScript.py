f211410d917525e60c0d5f4e857cedd2d87c9e99
AlonBenA <alonbareg@gmail.com>
2021-06-24 14:15:29 +0300

    first commit

---
+++ TestProjectCheck/Script/LoginScript.py
@@ -0,0 +1,11 @@
+﻿def LoginTest():
+    UserName = "SUPER_YAACOV"
+    Password = "Rafael6"
+    #Launches the specified browser and opens the specified URL in it.
+    Browsers.Item[btChrome].Run("https://rafael-test.wfsaas.com/workforce/Desktop.do")
+    #Enters text in the text box.
+    Aliases.browser.pageWorkforceTime.formLogonform.UserNameTextBox.SetText(UserName)
+    #Enters text in the text box.
+    Aliases.browser.pageWorkforceTime.formLogonform.PassWordTextBox.SetText(Password)
+    #Simulates a left-button single click in a window or control as specified (relative position, shift keys).
+    Aliases.browser.pageWorkforceTime.formLogonform.LoginButton.Click()
